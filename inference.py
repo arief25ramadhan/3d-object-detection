7fd436950e23ce0c0ae03d6edb49dd10d9485d38
arief <arief25ramadhan@yahoo.com>
2024-05-06 21:52:15 +0700

    Edit inference script

--- inference.py
+++ inference.py
@@ -1,11 +1,10 @@
-from mmdetecion3d.mmdet3d.apis import MonoDet3DInferencer
-from mmdet3d.apis import init_model, inference_detector
+import sys
+sys.path.append('mmdetecion3d/')
 
-
-# python demo/mono_det_demo.py demo/data/kitti/000008.png demo/data/kitti/000008.pkl  
-# configs/pgd/pgd_r101-caffe_fpn_head-gn_4xb3-4x_kitti-mono3d.py pgd_r101_caffe_fpn_gn-head_3x4_4x_kitti-mono3d_20211022_102608-8a97533b.pth --show --cam-type CAM2 --pred-score-thr 0.9
+from mmdet3d.apis import MonoDet3DInferencer
+from mmdet3d.apis import init_model, inference_detector, inference_mono_3d_detector
 
 config_file = 'mmdetection3d/configs/pgd/pgd_r101-caffe_fpn_head-gn_4xb3-4x_kitti-mono3d.py' # 'pointpillars_hv_secfpn_8xb6-160e_kitti-3d-car.py'
-checkpoint_file = 'pgd_r101_caffe_fpn_gn-head_3x4_4x_kitti-mono3d_20211022_102608-8a97533b.pth'
+checkpoint_file = 'mmdetection3d/pgd_r101_caffe_fpn_gn-head_3x4_4x_kitti-mono3d_20211022_102608-8a97533b.pth'
 model = init_model(config_file, checkpoint_file)
-inference_detector(model, 'demo/data/kitti/000008.png')
+inference_mono_3d_detector(model, 'mmdetection3d/demo/data/kitti/000008.png', 'mmdetection3d/demo/data/kitti/000008.pkl', cam_type='CAM2')
